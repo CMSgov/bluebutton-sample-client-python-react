7009688b7a0b4d97793423ff0187a52692db1543
Brady Fausett <brady.fausett@adhocteam.us>
2021-11-03 14:28:11 -0600

    Fixing docker issue with proxy and pipenv install

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3001',
+            target: 'http://server:3001',
             changeOrigin: true,
         })
     );
